3287d034b59f70d1431bf5a349521ecd5e4867a0
Egor Shcherba <egorshcherba94@gmail.com>
2023-04-01 23:02:39 +0300

    main :: added idt_init, gdt_init, pic_i8259_init

--- main.c
+++ main.c
@@ -1,6 +1,24 @@
+#define __DEBUG_HEADER__        "KERNEL"
+#define __DEBUG_HEADER_COLOR__  __FG_GREEN__
+#define __DEBUG_SUB_HEADER__    "MAIN"
+
+#include <debug/qemu.h>
+#include <sys/gdt.h>
+#include <sys/idt.h>
+#include <sys/pic_i8259.h>
+
+
 void
 main(void)
 {
+  dprintf("initilization...\n");
+
+  gdt_init();
+  idt_init();
+  pic_i8259_init();
+
+  dprintf("all subsystem initialized.\n");
+
   while (1)
     ;
 }
