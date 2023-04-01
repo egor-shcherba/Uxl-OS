8d715a3c5c11479fa9433a99e7ab64167c5d95da
Egor Shcherba <egorshcherba94@gmail.com>
2023-04-02 00:40:09 +0300

    debug :: added mem header

---
+++ debug/headers/mem.h
@@ -0,0 +1,9 @@
+#ifndef _DEBUG_HEADER_MEM_H
+#define _DEBUG_HEADER_MEM_H
+
+#define __DEBUG_HEADER__            "MEM"
+#define __DEBUG_CURRENT_LOG__       __LOG_MEMORY__
+#define __DEBUG_LOG_SUB_SYSTEM__    __DEBUG_SUB_MEMORY__
+#define __DEBUG_HEADER_COLOR__      __FG_WHITE__
+
+#endif /* NOT _DEBUG_HEADER_MEM_H */
