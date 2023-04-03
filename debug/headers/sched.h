c687a7190c042761e94a61b4e2fcafdc0b7ddc24
Egor Shcherba <egorshcherba94@gmail.com>
2023-04-03 20:26:26 +0300

    debug :: added debug header for scheduler

---
+++ debug/headers/sched.h
@@ -0,0 +1,9 @@
+#ifndef _DHEADER_SCHED_H
+#define _DHEADER_SCHED_H
+
+#define __DEBUG_HEADER__            "SCHED"
+#define __DEBUG_CURRENT_LOG__       __LOG_SCHED__
+#define __DEBUG_LOG_SUB_SYSTEM__    __DEBUG_SUB_SCHED__
+#define __DEBUG_HEADER_COLOR__      __FG_MAGENTA__
+
+#endif /* NOT _DHEADER_SCHED_H */
